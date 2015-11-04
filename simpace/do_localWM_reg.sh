62ca67e699a65679c97f8ee0bd62e551e74d789d
kai.hwang@gmail.com <kaihwang@nx5.priv>
2015-11-04 13:46:06 -0800

    changed sphere size, smooth and filter data (Jo et al).

--- simpace/do_localWM_reg.sh
+++ simpace/do_localWM_reg.sh
@@ -27,40 +27,48 @@ for session in sess01; do #$(ls -d sess*)
 	1d_tool.py -infile ${WD}/${session}/preproc/motion_params/MP.txt -select_rows '588..783' \
 	-write ${WD}/${session}/preproc/localWMreg/${session}_MP_run04.1D
 
+	#create WM mask
+	3dmask_tool -input ${WD}/${session}/preproc/wm_in_func_res/wm_func_res_0.85.nii.gz -dilate_inputs -1 -prefix ${WD}/${session}/preproc/localWMreg/${session}_WM_mask_erode1x.nii.gz
+		
+
+
 	for run in run01 run02 run03 run04; do
 
-		cd ${WD}/${session}/preproc/localWMreg/${session}
+		cd ${WD}/${session}/preproc/localWMreg/
 
-		#merge smoothed preproc files into one 4D nifit
-		fslmerge -t ${WD}/${session}/preproc/localWMreg/${session}_preproc_smooth_${run}.nii.gz $(ls ${WD}/${session}/preproc/smooth/*${run}*.nii)
+		#merge motion corrected preproc files into one 4D nifit
+		#gzip ${WD}/${session}/preproc/realign/asub01_${session}_${run}.nii 
+		fslmerge -t ${WD}/${session}/preproc/localWMreg/${session}_preproc_${run}.nii.gz $(ls ${WD}/${session}/preproc/realign/*${run}*.nii.gz)
 		
 		# fix TR info
-		3drefit -TR 2 ${WD}/${session}/preproc/localWMreg/${session}_preproc_smooth_${run}.nii.gz 
+		3drefit -TR 2 ${WD}/${session}/preproc/localWMreg/${session}_preproc_${run}.nii.gz 
 
 		#get csf regressors
 		3dmaskave -mask ${WD}/${session}/preproc/csf_in_func_res/csf_func_res_final.nii.gz -quiet \
-		${WD}/${session}/preproc/localWMreg/${session}_preproc_smooth_${run}.nii.gz > ${WD}/${session}/preproc/localWMreg/${session}_csf_${run}.1D
+		${WD}/${session}/preproc/localWMreg/${session}_preproc_${run}.nii.gz > ${WD}/${session}/preproc/localWMreg/${session}_csf_${run}.1D
 
-		#get local WM regressors
+		#get local WM regressors	
 		3dLocalstat -prefix ${WD}/${session}/preproc/localWMreg/${session}_WMeLOCAL_${run}.nii.gz \
-		-nbhd 'SPHERE(30)' \
+		-nbhd 'SPHERE(50)' \
 		-stat mean \
-		-mask ${WD}/${session}/preproc/wm_in_func_res/wm_func_res_final.nii.gz \
-		-use_nonmask ${WD}/${session}/preproc/localWMreg/${session}_preproc_smooth_${run}.nii.gz
+		-mask ${WD}/${session}/preproc/localWMreg/${session}_WM_mask_erode1x.nii.gz \
+		-use_nonmask ${WD}/${session}/preproc/localWMreg/${session}_preproc_${run}.nii.gz
 
 
 		##do regression
 		#the model at this point is Y = [ Motion + localWMreg + Ventricles ] X + Residual 
-		# did not remove linear drift
-		# bandpass filtering at 0.008 0.09
+		# bandpass filtering at 0.01 0.08, simultaneously with regression
+		# spatial smooth after regression
 		3dTproject \
-		-input ${WD}/${session}/preproc/localWMreg/${session}_preproc_smooth_${run}.nii.gz \
-		-prefix ${WD}/${session}/preproc/localWMreg/${session}_preproc_smooth_localWMreg_${run}.nii.gz \
+		-input ${WD}/${session}/preproc/localWMreg/${session}_preproc_${run}.nii.gz \
+		-prefix ${WD}/${session}/preproc/localWMreg/${session}_preproc_localWMreg_${run}.nii.gz \
 		-ort ${WD}/${session}/preproc/localWMreg/${session}_MP_${run}.1D \
 		-ort ${WD}/${session}/preproc/localWMreg/${session}_csf_${run}.1D \
 		-dsort ${WD}/${session}/preproc/localWMreg/${session}_WMeLOCAL_${run}.nii.gz \
 		-automask \
-		-passband 0.008 0.09
+		-passband 0.01 0.08 \
+		-blur 6 \
+		-polort 2
 
 	done
 done
