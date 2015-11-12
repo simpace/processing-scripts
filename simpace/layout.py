d051575c87072505ddcf7df147f203febc66f26e
JB Poline <jbpoline@gmail.com>
2015-11-11 19:17:41 -0500

    documenting the layout

--- simpace/layout.py
+++ simpace/layout.py
@@ -1,10 +1,62 @@
 from __future__ import print_function, division
 import os 
+import errno
 import os.path as osp
 import json
 import re
 import glob as gb
 from six import string_types
+
+
+# Documentation:
+
+"""
+The json file contains a set of keys that are the entities that we wish to retrieve.
+List of Json file top level keys, also called entities: 
+                        ["subjects", 
+                         "sessions", 
+                         "runs", 
+                         "smoothed", 
+                         "mvt6params", 
+                         ...]
+
+Each entity will contain a set of (ek,v) pairs. ek can be  
+    'base'      : if we want a path, this is the last added bit
+    'pth'       : made with a set of entities, get the ek,val,base  
+    'hasdirs'   : determine if there is another level to the path
+    'key'       : used to construct both path and filenames
+    'val'       : used to construct both path and filenames
+    'glb'       : list of stuff to construct filenames to glob
+                    contains 'pre' 'post' 'lnk' and list of ek
+    'pre' 'post' 'lnk' : use to construc glb
+
+
+A entity will contain one of several files or directory, or values.
+There are two main usages:
+    1. We want to construct the path that leads to a specific file. To do this, 
+    we use the k-v: base, pth, hasdir, val, key. 
+    The path is constructed with the entities in pth, for each we add : 
+        /   'key' + val + base / 'key' + val + base / ...
+    If `glob` is True, then val are replaced by '*' and the result can be use to 
+    glob the pattern
+
+    2. We want to construct a pattern to glob some *files*
+    This takes the glb list and constructs a series of
+        dlayo[k]['key'] + dlayo[k]['val'] + dlayo[k]['lnk']
+
+
+  "smoothed": {
+    "base": "smooth",
+    "pth": ["subjects", "sessions", "runs"],
+    "hasdirs": false,
+    "pre": "sra",
+    "post": "????.nii*",
+    "glb": ["pre", "subjects", "sessions", "runs", "post"]
+  },
+
+
+"""
+
 
 def _get_key_dict(dlayo, entities = ["subjects", "sessions", "runs"]):
     """
@@ -71,6 +123,8 @@ def _format(toformat, fdict, failsifnotcomplete=True):
         pass
     return toformat.format(**fdict)
 
+
+
 #    reg = '\{([a-z:]*)\}' #should capture the {aa:bb} in the string
 #    p = re.compile(reg)
 #    args = p.findall(toformat)
@@ -80,9 +134,8 @@ def _format(toformat, fdict, failsifnotcomplete=True):
 #            k,v = arg.split(':')
 #            assert k in fdict
 #            reduced_fdic[k] = fdict[k]
-#    
 
-def _get_pth(dlayo, key, verbose=False):
+def _get_pth(dlayo, key, glob=False, verbose=False):
     """
     construct the path, the file name, and the dictionary of keys for the path  
     returns
@@ -94,7 +147,8 @@ def _get_pth(dlayo, key, verbose=False):
     try:
         assert key in dlayo
     except:
-        print("Key {} not in dict keys {}".format(key, dlayo.keys()))
+        err_msg = "Key {} not in dict keys {}".format(key, dlayo.keys())
+        raise ValueError, err_msg
 
     strpth = dlayo['base'] 
     pdic = {}
@@ -103,23 +157,41 @@ def _get_pth(dlayo, key, verbose=False):
         base = dlayo[p]['base']
         add_dir = ''
         if 'hasdirs' in dlayo[p] and (dlayo[p]['hasdirs'] is True): 
+            if glob:
+                val = '*'
+            else:
+                val = dlayo[p]['val']
             # add the k-v liason in the future 
-            add_dir = dlayo[p]['key']+dlayo[p]['val']
+            add_dir = dlayo[p]['key'] + val
             pdic[dlayo[p]['key']] = None
         strpth = osp.join(strpth, base, add_dir)
         
     # then add the base of this specific key:
     strpth = osp.join(strpth, dlayo[key]['base'])
 
-    return strpth, pdic 
+    if glob: 
+        return strpth
+    else:
+        return strpth, pdic 
 
-
-def _get_glb(dlayo, key, verbose=False):
+def _get_glb(dlayo, key, glob=False, verbose=False):
     """
-    construct the path, the file name, and the dictionary of keys for the file name 
+    This constructs a string from the "glb" key that can be used to glob
+    It constructs only the file name(s), but not the path to this / these 
+    file(s). It also returns a dictionary of keys to be included
+    for the file name 
+    parameters:
+    -----------
+    dlayo: dict
+        the dictionary that contains the layout from a json file
+    key: string
+        the entity we are interested in 
+    glob: boolean
+        return a string that I can glob ?
+
     returns:
     --------
-    the string corresponding to the glob pattern 
+    the string to be use to glob the pattern 
     """
     try:
         assert key in dlayo
@@ -127,42 +199,76 @@ def _get_glb(dlayo, key, verbose=False):
         print("Key {} not in dict keys {}".format(key, dlayo.keys()))
 
     if 'glb' not in dlayo[key]:
-        raise ValueError(" 'glb' is not a key for '{}'".format(key))
+        raise ValueError(" 'glb' is not a key for '{}', ".format(key) + 
+                         " I cannot _get_glb for that key ")
 
     # return string if simply a string
     if type(dlayo[key]['glb']) in (str, unicode):
-        return dlayo[key]['glb'], {}
+         strglb, fdic = dlayo[key]['glb'], {}
+    else:
+        # construct file name using the 'glb' section of dlayo[key] 
+        strglb = ''
+        fdic = {}
+        for k in dlayo[key]['glb']:
+            if k in dlayo[key]:
+                strglb += dlayo[key][k]
+            elif k in dlayo:
+                if glob:
+                    val = '*'
+                else:
+                    val = dlayo[k]['val']
+                #here : for the last k, we may not want to put the lnk
+                strglb += dlayo[k]['key'] + val + dlayo[k]['lnk']
+                fdic[dlayo[k]['key']] = None
+            else:
+                raise ValueError, "entity: k {} key {} unknown".format(k, key)
 
-    # construct file name using the 'glb' section of dlayo[key] 
-    strglb = ''
-    fdic = {}
-    for k in dlayo[key]['glb']:
-        if k in dlayo[key]:
-            strglb += dlayo[key][k]
-        elif k in dlayo:
-            strglb += dlayo[k]['key'] + dlayo[k]['val'] + dlayo[k]['lnk']
-            fdic[dlayo[k]['key']] = None
-        else:
-            raise ValueError, "k {} key {} unknown".format(k, key)
+    if glob: 
+        return strglb
+    else:
+        return strglb, fdic 
 
-    return strglb, fdic
-
-def _get_pthglb(dlayo, key, verbose=False):
+def _get_pthglb(dlayo, key, glob=False, verbose=False):
     """
-    """
-    pth, pth_dict = _get_pth(dlayo, key, verbose=verbose)
-    glb, glb_dict = _get_glb(dlayo, key, verbose=verbose)
+    parameters:
+    -----------
+    dlayo: dict
+        the dictionary that contains the layout from a json file
+    key: string
+        the entity we are interested in 
+    glob: boolean
+        return a string that I can glob ?
 
-    pthglb_dict = pth_dict.copy()
-    pthglb_dict.update(glb_dict)
+    returns:
+    --------
 
-    return osp.join(pth, glb), pthglb_dict
 
-def _get_apthglb(dlayo, key, dstate, verbose=False):
     """
+    if glob:
+        pth = _get_pth(dlayo, key, glob=glob, verbose=verbose)
+        glb = _get_glb(dlayo, key, glob=glob, verbose=verbose)
+        return osp.join(pth, glb), None
+    else:
+        pth, pth_dict = _get_pth(dlayo, key, glob=glob, verbose=verbose)
+        glb, glb_dict = _get_glb(dlayo, key, glob=glob, verbose=verbose)
+        pthglb_dict = pth_dict.copy()
+        pthglb_dict.update(glb_dict)
+        return osp.join(pth, glb), pthglb_dict
+
+
+def _get_apthglb(dlayo, key, dstate, glob=False, verbose=False):
     """
-    pthglb, _ = _get_pthglb(dlayo, key, verbose=verbose)
-    return osp.join(pthglb).format(**dstate)
+    get the pth and glob, then instanciate this with the dstate
+
+    """
+    pthglb, _ = _get_pthglb(dlayo, key, glob=glob, verbose=verbose)
+    try:
+        apthglb = osp.join(pthglb).format(**dstate)
+    except:
+       raise ValueError(  " pthglb is {} ".format(pthglb) 
+                        + " dstate is {} ".format(dstate)
+                        + " and pthglb cannot be formated with dstate.")
+    return apthglb
 
 def _get_apth(dlayo, key, dstate, verbose=False):
     """
@@ -170,7 +276,6 @@ def _get_apth(dlayo, key, dstate, verbose=False):
     """
     pth, _ = _get_pth(dlayo, key, verbose=verbose)
     return osp.join(pth).format(**dstate)
-
 
 def _get_oneof(dlayo, key, verbose=False):
     """
@@ -218,19 +323,25 @@ def _get_alistof(dlayo, key, dstate={}, return_idxs=False, verbose=False):
         # raise ValueError("{} has not multiple dirs".format(key))
 
 def _get_aunique(dlayo, key, dstate={}, check_exists=True):
-
-    apthglb = _get_apthglb(dlayo, key, dstate, verbose=False)
+    """
+    first _get_aphtglb then check if only one element (file) is returned
+    """
+    apthglb = _get_apthglb(dlayo, key, dstate, glob=True, verbose=False)
     files = gb.glob(apthglb)
     if len(files) != 1:
-        raise ValueError(" key '{}' is suppose to yield only one file".format(key))
+        raise ValueError(" key '{}' is suppose to yield exactly one file".format(key) +
+                         "\n and is : {}".format(files) + 
+                         "\n with apthglb on pattern {}".format(apthglb))
     else:
         if check_exists:
             if not osp.isfile(files[0]):
-                raise ValueError(" file/dir '{}' does not exist".format(files[0]))
+                raise ValueError(" file/dir: '{}' does not exist".format(files[0]) +
+                                 "\n with glob on pattern {}".format(apthglb))
     return files[0]
 
 
-    
+
+#--------------------------------------- a few utilities functions ---------------# 
 
 def _check_dict_full(adic, verbose=False):
     """
@@ -240,7 +351,20 @@ def _check_dict_full(adic, verbose=False):
     return True
 
 
+def merge_two_dicts(x, y):
+    '''Given two dicts, merge them into a new dict as a shallow copy.'''
+    z = x.copy()
+    z.update(y)
+    return z
 
+
+def symlink_force(target, link_name):
+    try:
+        os.symlink(target, link_name)
+    except OSError, e:
+        if e.errno == errno.EEXIST:
+            os.remove(link_name)
+            os.symlink(target, link_name)
 
 
 # def get_section(dlayo, section, verbose=False):
@@ -299,4 +423,8 @@ def _check_dict_full(adic, verbose=False):
 
 
 
+#    
+#    
+#    
+#    
 
