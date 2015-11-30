d97eb642f2470be88bbd40b1b6ac5f6e3d21ab0f
JB Poline <jbpoline@gmail.com>
2015-11-29 17:12:42 -0800

    script for analyzing other NR preprocessed data

---
+++ simpace/all_preprocessed.py
@@ -0,0 +1,70 @@
+from __future__ import print_function
+
+import os.path as osp
+import json
+import argparse
+import smpce_data_to_corr as stc
+
+def main(dbase, json_files, verbose=False):
+
+    """
+    """
+    # read common params
+    params = stc.get_params(dbase, verbose=verbose)
+    cwd = osp.dirname(osp.realpath(__file__))
+
+    # add what is in the additional json files
+    for json_file in json_files: 
+        if verbose: print("adding json file {}".format(json_file))
+
+        with open(json_file) as fjson:
+            djson = json.load(fjson)
+        for djson_k in djson:
+            params['layout'][djson_k] = djson[djson_k]
+            if verbose: print(djson_k, djson[djson_k])
+
+        info = stc.process_all("dummy_dbase", params, verbose=verbose)
+
+        ftmp = osp.join(cwd,'tmp_' + osp.join(osp.basename(json_file),'log'))
+
+        with open(ftmp, 'w') as this_file: 
+            print(info, file=this_file)
+
+
+#------------------------------------------------------------------------------
+# Running from command line
+#------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    #
+    parser = argparse.ArgumentParser()
+
+    # Positional required arguments
+    help_base_dir = "The directory containing sub01/sess??/... and json files: \n" + \
+                    "analysis_parameters.json  data_parameters.json  directory_layout.json"
+    parser.add_argument("base_directory", help=help_base_dir)
+
+    # Optional arguments
+
+    help_add_json = "an additional json layout file overwritting the one found in the base dir"
+
+    parser.add_argument('-a','--add_jsons', nargs='+', help=help_add_json, required=False)
+
+    parser.add_argument("--verbose", help="increase output verbosity", action="store_true")
+
+    args = parser.parse_args()
+    base_dir = args.base_directory
+    add_jsons = args.add_jsons
+    verbose = args.verbose
+    
+    print("launching analyses on :", base_dir)
+    assert osp.isdir(base_dir), '{} not a directory'.format(base_dir)
+
+    info = main(base_dir, add_jsons, verbose=verbose)
+     
+    if verbose:
+        print("\n------------------- Debug info ------------------ \n")
+        print(info)
+        print("\n------------------- Debug info ------------------ \n")
+
+
