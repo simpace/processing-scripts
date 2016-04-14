fbc77b91581843c52933bdb420d9088a8305bf5a
Arielle Tambini <arielle.tambini@gmail.com>
2016-04-13 19:10:59 -0700

    nipype workflows/preprocessing w BIDS file organization

---
+++ simpace/preproc/Pmaps_v3.py
@@ -0,0 +1,70 @@
+import os, glob, sys
+from os.path import join as pjoin
+import nipype.interfaces.fsl as fsl
+import numpy as np
+import SimpleITK as sitk
+import nibabel as nb
+
+workflow_dir = sys.argv[1]
+
+file_ext = '.nii'
+gzip_ext = '.nii.gz'
+
+print 'eroding masks for session: ' + workflow_dir
+
+folder_base = '_in_func_res'
+masks = ['wm', 'csf']
+
+Nvoxels_min = 20  # minimum # of voxels required after mask erosion
+
+probability_thrs = np.arange(.80, .99, .01)
+
+imagemaths = fsl.ImageMaths()
+
+erodeITK = sitk.BinaryErodeImageFilter()
+erodeITK.SetKernelRadius(1)
+erodeITK.SetKernelType(3)
+erodeITK.SetForegroundValue(1)
+
+for imask in masks:
+
+    mask_path = pjoin(workflow_dir, imask + folder_base)
+    file_path = mask_path + '/' + imask + '*' + file_ext
+    mask_file = glob.glob(file_path)[0]
+    mask_base = os.path.splitext(os.path.basename(mask_file))[0]
+
+    final_mask_file = mask_path + '/' + mask_base + '_final' + gzip_ext
+
+    for ithresh in probability_thrs:
+
+        new_file_base = mask_path + '/' + mask_base + '_' + str(ithresh)
+        thresh_file = new_file_base + gzip_ext
+        erode_file = new_file_base + '_erode' + gzip_ext
+
+        imagemaths.inputs.in_file = mask_file
+        imagemaths.inputs.op_string = '-thr ' + str(ithresh) + ' -bin'  # Threshold & binarize
+        imagemaths.inputs.out_file = thresh_file
+        imagemaths.run()
+
+        img = sitk.ReadImage(thresh_file)
+        img = sitk.Cast(img, sitk.sitkInt8)
+
+        out = erodeITK.Execute(img)
+
+        if imask == 'wm':  # erode twice
+            out_again = erodeITK.Execute(out)
+            sitk.WriteImage(out_again, erode_file)
+
+        elif imask == 'csf':  # erode once
+            sitk.WriteImage(out, erode_file)
+
+        output_mask = nb.load(erode_file).get_data()
+        total = np.sum(output_mask)
+
+        if total >= Nvoxels_min:
+            print str(total) + ' voxels in ' + erode_file
+
+            if os.path.islink(final_mask_file):
+                os.remove(final_mask_file)
+
+            os.symlink(os.path.splitext(os.path.basename(erode_file))[0] + '.gz', final_mask_file)
