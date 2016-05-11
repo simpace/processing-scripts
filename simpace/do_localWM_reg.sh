e5f875edd2be69c08e143f43d6b787a2e2cfabb6
Kai Hwang <kai.hwang@gmail.com>
2016-05-11 11:18:53 -0700

    update file paths for the new file structure.

--- simpace/do_localWM_reg.sh
+++ simpace/do_localWM_reg.sh
@@ -1,81 +1,84 @@
 # script to run AFNI's ANATICOR (local WM regression) on simpace data
 
 
-WD='/home/despo/simpace/rename_files/sub01/'
-cd ${WD}
-for session in $(ls -d sess*); do #$(ls -d sess*)
-	
-	cd ${WD}/${session}/preproc
+for sub in sub-01 sub-02; do
 
-	# where to put stuff
-	if [ ! -d ${WD}/${session}/preproc/localWMreg ]; then
-		mkdir ${WD}/${session}/preproc/localWMreg/
-	fi
-	
+	WD="/home/despo/simpace/rename_files/derivatives/${sub}/"
 
-	#split motion regressors
-	cd ${WD}/${session}/preproc/motion_params/
-	ln -s ${WD}/${session}/preproc/motion_params/MP_run01.txt ${WD}/${session}/preproc/localWMreg/${session}_MP_run01.1D
-	ln -s ${WD}/${session}/preproc/motion_params/MP_run02.txt ${WD}/${session}/preproc/localWMreg/${session}_MP_run02.1D
-	ln -s ${WD}/${session}/preproc/motion_params/MP_run03.txt ${WD}/${session}/preproc/localWMreg/${session}_MP_run03.1D
-	ln -s ${WD}/${session}/preproc/motion_params/MP_run04.txt ${WD}/${session}/preproc/localWMreg/${session}_MP_run04.1D
-
-
-	#create WM mask
-	if [ ! -e ${WD}/${session}/preproc/localWMreg/${session}_WM_mask_erode1x.nii.gz ]; then
-		3dmask_tool -input ${WD}/${session}/preproc/wm_in_func_res/wm_func_res_0.85.nii.gz -dilate_inputs -1 \
-		-prefix ${WD}/${session}/preproc/localWMreg/${session}_WM_mask_erode1x.nii.gz
-	fi	
-
-
-	for run in run01 run02 run03 run04; do
-
-		cd ${WD}/${session}/preproc/localWMreg/
-
-		#merge motion corrected preproc files into one 4D nifit
-		#gzip ${WD}/${session}/preproc/realign/asub01_${session}_${run}.nii 
-		if [ ! -e ${WD}/${session}/preproc/localWMreg/${session}_preproc_${run}.nii.gz ]; then
-			fslmerge -t ${WD}/${session}/preproc/localWMreg/${session}_preproc_${run}.nii.gz $(ls ${WD}/${session}/preproc/realign/*${run}*.nii.gz)
-			# fix TR info
-			3drefit -TR 2 ${WD}/${session}/preproc/localWMreg/${session}_preproc_${run}.nii.gz 
-		fi
-
+	cd ${WD}
+	for session in $(/bin/ls -d ses-t*); do #$(ls -d sess*)
 		
-		#get csf regressors
-		3dmaskave -mask ${WD}/${session}/preproc/csf_in_func_res/csf_func_res_final.nii.gz -quiet \
-		${WD}/${session}/preproc/localWMreg/${session}_preproc_${run}.nii.gz > ${WD}/${session}/preproc/localWMreg/${session}_csf_${run}.1D
+		cd ${WD}/${session}/func_proc
 
-		#get global signal
-		#3dmaskave -mask ${WD}/${session}/preproc/sess_mask/sess_mask.nii -quiet \
-		#${WD}/${session}/preproc/localWMreg/${session}_preproc_${run}.nii.gz > ${WD}/${session}/preproc/localWMreg/${session}_GS_${run}.1D
+		rm -rf ${WD}/${session}/func_proc/localWMreg/
+		# where to put stuff
+		if [ ! -d ${WD}/${session}/func_proc/localWMreg ]; then
+			mkdir ${WD}/${session}/func_proc/localWMreg/
+		fi
+		
 
+		#split motion regressors
+		#cd ${WD}/${session}/func_proc/motion_params/
+		ln -s ${WD}/${session}/func_proc/motion_params/MP_NONE.txt ${WD}/${session}/func_proc/localWMreg/${session}_MP_NONE.1D
+		ln -s ${WD}/${session}/func_proc/motion_params/MP_LOW.txt ${WD}/${session}/func_proc/localWMreg/${session}_MP_LOW.1D
+		ln -s ${WD}/${session}/func_proc/motion_params/MP_MED.txt ${WD}/${session}/func_proc/localWMreg/${session}_MP_MED.1D
+		ln -s ${WD}/${session}/func_proc/motion_params/MP_HIGH.txt ${WD}/${session}/func_proc/localWMreg/${session}_MP_HIGH.1D
 
-		#get local WM regressors	
-		if [ ! -e ${WD}/${session}/preproc/localWMreg/${session}_WMeLOCAL_${run}.nii.gz ]; then
-			3dLocalstat -prefix ${WD}/${session}/preproc/localWMreg/${session}_WMeLOCAL_${run}.nii.gz \
-			-nbhd 'SPHERE(50)' \
-			-stat mean \
-			-mask ${WD}/${session}/preproc/localWMreg/${session}_WM_mask_erode1x.nii.gz \
-			-use_nonmask ${WD}/${session}/preproc/localWMreg/${session}_preproc_${run}.nii.gz
+		#create WM mask
+		if [ ! -e ${WD}/${session}/func_proc/localWMreg/${session}_WM_mask_erode1x.nii.gz ]; then
+			3dmask_tool -input ${WD}/${session}/func_proc/wm_in_func_res/wm_func_res_0.85.nii.gz -dilate_inputs -1 \
+			-prefix ${WD}/${session}/func_proc/localWMreg/${session}_WM_mask_erode1x.nii.gz
 		fi	
 
-		##do regression
-		#the model at this point is Y = [ Motion + localWMreg + Ventricles ] X + Residual 
-		# bandpass filtering at 0.01 0.08, simultaneously with regression
-		# spatial smooth after regression
-		
-		if [ ! -e ${WD}/${session}/preproc/localWMreg/${session}_preproc_localWMreg_${run}.nii.gz ]; then
-			3dTproject \
-			-input ${WD}/${session}/preproc/localWMreg/${session}_preproc_${run}.nii.gz \
-			-prefix ${WD}/${session}/preproc/localWMreg/${session}_preproc_localWMreg_${run}.nii.gz \
-			-ort ${WD}/${session}/preproc/localWMreg/${session}_MP_${run}.1D \
-			-ort ${WD}/${session}/preproc/localWMreg/${session}_csf_${run}.1D \
-			-dsort ${WD}/${session}/preproc/localWMreg/${session}_WMeLOCAL_${run}.nii.gz \
-			-automask \
-			-passband 0.01 0.08 \
-			-blur 6 \
-			-polort 2
-		fi
 
+		for run in NONE LOW MED HIGH; do
+
+			cd ${WD}/${session}/func_proc/localWMreg/
+
+			#merge motion corrected preproc files into one 4D nifit
+			#gzip ${WD}/${session}/func_proc/realign/asub01_${session}_${run}.nii 
+			if [ ! -e ${WD}/${session}/func_proc/localWMreg/${session}_preproc_${run}.nii.gz.gz ]; then
+				#fslmerge -t ${WD}/${session}/func_proc/localWMreg/${session}_preproc_${run}.nii.gz $(ls ${WD}/${session}/func_proc/smooth/*${run}*.nii)
+				# fix TR info
+				#3drefit -TR 2 ${WD}/${session}/func_proc/localWMreg/${session}_preproc_${run}.nii.gz 
+				ln -s ${WD}/${session}/func_proc/realign/ra${sub}_${session}_task-rest_acq-${run}_bold.nii.gz ${WD}/${session}/func_proc/localWMreg/${session}_preproc_${run}.nii.gz
+			fi
+
+			
+			#get csf regressors
+			3dmaskave -mask ${WD}/${session}/func_proc/csf_in_func_res/csf_func_res_final.nii.gz -quiet \
+			${WD}/${session}/func_proc/localWMreg/${session}_preproc_${run}.nii.gz > ${WD}/${session}/func_proc/localWMreg/${session}_csf_${run}.1D
+
+			#get global signal
+			3dmaskave -mask ${WD}/${session}/func_proc/sess_mask/sess_mask.nii -quiet \
+			${WD}/${session}/func_proc/localWMreg/${session}_preproc_${run}.nii.gz > ${WD}/${session}/func_proc/localWMreg/${session}_${run}.1D
+
+			#get local WM regressors	
+			if [ ! -e ${WD}/${session}/func_proc/localWMreg/${session}_WMeLOCAL_${run}.nii.gz ]; then
+				3dLocalstat -prefix ${WD}/${session}/func_proc/localWMreg/${session}_WMeLOCAL_${run}.nii.gz \
+				-nbhd 'SPHERE(50)' \
+				-stat mean \
+				-mask ${WD}/${session}/func_proc/localWMreg/${session}_WM_mask_erode1x.nii.gz \
+				-use_nonmask ${WD}/${session}/func_proc/localWMreg/${session}_preproc_${run}.nii.gz
+			fi	
+
+			##do regression
+			#the model at this point is Y = [ Motion + localWMreg + Ventricles ] X + Residual 
+			# bandpass filtering at 0.01 0.08, simultaneously with regression
+			# spatial smooth after regression
+			
+			if [ ! -e ${WD}/${session}/func_proc/localWMreg/${session}_preproc_localWMreg_${run}.nii.gz ]; then
+				3dTproject \
+				-input ${WD}/${session}/func_proc/localWMreg/${session}_preproc_${run}.nii.gz \
+				-prefix ${WD}/${session}/func_proc/localWMreg/${session}_preproc_localWMreg_${run}.nii.gz \
+				-ort ${WD}/${session}/func_proc/localWMreg/${session}_MP_${run}.1D \
+				-ort ${WD}/${session}/func_proc/localWMreg/${session}_csf_${run}.1D \
+				-dsort ${WD}/${session}/func_proc/localWMreg/${session}_WMeLOCAL_${run}.nii.gz \
+				-automask \
+				-passband 0.01 0.08 \
+				-polort 2
+			fi
+
+		done
 	done
 done
